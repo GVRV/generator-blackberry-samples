19b0891faf0d33d76b1925958dfbe0824d737c7e
Gaurav Dadhania <gaurav@dadhania.in>
2013-02-17 02:45:18 +1100

    New app.js format

--- identity/app.js
+++ identity/app.js
@@ -1,9 +1,3 @@
-function ready() {
-    var ele = document.createElement("div");
-    ele.innerHTML = "uuid: " + blackberry.identity.uuid;
-    document.documentElement.appendChild(ele);
-}
-
-window.addEventListener("load", function(e) {
-    document.addEventListener("webworksready", ready);
-}, false);
+var ele = document.createElement("div");
+ele.innerHTML = "uuid: " + blackberry.identity.uuid;
+document.documentElement.appendChild(ele);
